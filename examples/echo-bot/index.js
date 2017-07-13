fc3227520b58f30c36390cda597f5047cca72a26
Chan Chan <chan2chan.2009@gmail.com>
2017-07-13 07:29:16 +0000

    Update index.js

--- examples/echo-bot/index.js
+++ examples/echo-bot/index.js
@@ -5,8 +5,8 @@ const express = require('express');
 
 // create LINE SDK config from env variables
 const config = {
-  channelAccessToken: 'UymbUV9vT9UItuFsQdHuDlFug3gI/JayqcDDgwsrkyjvJSkdMAQ0SAnhOuu7kc2ysvO5ucU1mFUpfxg51AuSpso1PoaK0bDKLNCSGO2PeM6fq69SZ8zDwR/Krako/BsDoaAvvV+manIriQQddlHfkgdB04t89/1O/w1cDnyilFU=',
-  channelSecret: 'a46ba5bc771ed42c42dc18f2b5750c1b',
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
 };
 
 // create LINE SDK client
